cf57e141b72600a1919e80bdb5558ef1a897c0c5
Md Rezaul Karim <diureza@gmail.com>
2022-03-21 20:34:28 +0600

    update for chat npm

--- client/src/admin/views/dashboard/section/budget.tsx
+++ client/src/admin/views/dashboard/section/budget.tsx
@@ -1,13 +1,7 @@
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import MoneyIcon from "@mui/icons-material/Money";
-import {
-  Avatar,
-  Box,
-  Card,
-  CardContent,
-  Grid,
-  Typography,
-} from "@mui/material";
 export default function Budget() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>budget</h1>
+    </div>
+  );
 }
